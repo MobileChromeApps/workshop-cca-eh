9cd5e5409dfae0328bdf02050f195d89e9e45371
Ian Clelland <iclelland@chromium.org>
2014-08-22 11:00:18 -0400

    Play a "ping" on notification receipt

--- www/answers/chrome.notifications.answers.js
+++ www/answers/chrome.notifications.answers.js
@@ -25,6 +25,29 @@ function createLocalNotification(notificationId, options, callback) {
   }
   options.message = options.message || 'Eh';
   chrome.notifications.create(notificationId, options, callback || function() {});
+  play("assets/sounds/ASDIC.wav");
 }
 
 /******************************************************************************/
+
+function play(url) {
+  var request = new XMLHttpRequest();
+
+  request.open('GET', url, true)
+  request.responseType = "arraybuffer"
+
+  request.onload = function() {
+    var audioCtx = new window.AudioContext;
+    audioCtx.decodeAudioData(request.response, function(buffer) {
+      var source = audioCtx.createBufferSource();
+      source.buffer = buffer;
+      source.connect(audioCtx.destination);
+      source.loop = false;
+      source.start(0);
+    }, function(e) {
+      console.log("Error decoding audio data" + e.err)
+    });
+  }
+
+  request.send()
+}
