7149f8c9fd2040fb370d6899045aeda4f2c2c52c
Michal Mocny <mmocny@gmail.com>
2014-09-30 15:38:32 -0400

    Restructuring with latest gcmServer updates

---
+++ gcmServer/node/xmpp_gcm_server/main.js
@@ -0,0 +1,18 @@
+var xmpp = require('node-xmpp-client');
+
+var options = {
+  type: 'client',
+  jid: '90031296475@gcm.googleapis.com',
+  password: 'AIzaSyBa5T4FvOUgxKLHyzTf13FLw9UU02GR3Lc',
+  port: 5235,
+  host: 'gcm.googleapis.com',
+  legacySSL: true,
+  preferredSaslMechanism : 'PLAIN'
+};
+
+var cl = new xmpp.Client(options);
+
+cl.on('online', function()
+{
+    console.log("XMPP Online");
+});
