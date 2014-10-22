903a2e6dd9630dc7a7181d99fe095eab3b1aa5ff
Sam Thorogood <sam.thorogood@gmail.com>
2014-10-22 20:15:50 +1100

    comment

--- workshop/copy_app_common_to_steps.sh
+++ workshop/copy_app_common_to_steps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script copies the known good app/ui folder into each workshop step. It
+# should be rerun after any updates to the common Polymer elements and assets.
 
 set -eu
 cd "$(dirname "$0")"
